fa76720bd18659f9723cf7f6e44edbe011451803
SergeyYanushkevichA1QA <75015141+SergeyYanushkevichA1QA@users.noreply.github.com>
2021-04-09 13:31:38 +0300

    refactor#1

--- src/test/java/by/a1qa/tests/BaseTest.java
+++ src/test/java/by/a1qa/tests/BaseTest.java
@@ -1,16 +1,17 @@
 package by.a1qa.tests;
 
 import aquality.appium.mobile.application.AqualityServices;
+import aquality.selenium.core.logging.Logger;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 
 
 public class BaseTest {
+    public final static Logger logger = AqualityServices.getLogger();
 
     @BeforeClass
     public void setup(){
-        System.setProperty("profile", "browserstack");
         AqualityServices.getApplication();
         AqualityServices.getLogger().info("App is opened");
     }
@@ -20,7 +21,6 @@ public class BaseTest {
         if (AqualityServices.isApplicationStarted())
             AqualityServices.getApplication().getDriver().quit();
         AqualityServices.getLogger().info("App is closed");
-        System.clearProperty("profile");
     }
 }
 
